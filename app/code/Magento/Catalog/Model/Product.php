a004cd225ee453f77e82a604eef4832cb0abef73
Leandry <leandry@atwix.com>
2020-05-01 10:52:26 +0200

    DEVOPS-490 NEW TEST

--- app/code/Magento/Catalog/Model/Product.php
+++ app/code/Magento/Catalog/Model/Product.php
@@ -1203,7 +1203,6 @@ class Product extends \Magento\Catalog\Model\AbstractModel implements
     {
         $this->_data['final_price'] = $price;
         return $this;
-        console.log($this);
     }
 
     /**
